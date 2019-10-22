71fc27ec5a48835b5c1c38eacd967354382fa560
terrypg34 <56301490+terrypg34@users.noreply.github.com>
2019-10-22 21:31:02 +0200

    [ADD] Window Build Game

---
+++ Build Croissance/Croissance_WwiseIntegrationTemp1_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,89 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSIC_CONTEMPLATION = 2990463502U;
+        static const AkUniqueID MUSIC_DEGRADATION_0 = 1507659042U;
+        static const AkUniqueID MUSIC_DEGRADATION_1 = 1507659043U;
+        static const AkUniqueID MUSIC_DEGRADATION_2 = 1507659040U;
+        static const AkUniqueID MUSIC_DEGRADATION_3 = 1507659041U;
+        static const AkUniqueID MUSIC_DEGRADATION_4 = 1507659046U;
+        static const AkUniqueID MUSIC_KILL_CORAL = 41896295U;
+        static const AkUniqueID MUSIC_PHOTO = 2199501515U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_DEGRADATION
+        {
+            static const AkUniqueID GROUP = 1532608259U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DEGRADATION_0 = 2065453594U;
+                static const AkUniqueID DEGRADATION_1 = 2065453595U;
+                static const AkUniqueID DEGRADATION_2 = 2065453592U;
+                static const AkUniqueID DEGRADATION_3 = 2065453593U;
+                static const AkUniqueID DEGRADATION_4 = 2065453598U;
+            } // namespace STATE
+        } // namespace MUSIC_DEGRADATION
+
+        namespace MUSIC_ST
+        {
+            static const AkUniqueID GROUP = 2996874600U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CONTEMPLATION = 3810549462U;
+                static const AkUniqueID KILL_CORAL = 981024095U;
+                static const AkUniqueID PHOTO = 3205522195U;
+            } // namespace STATE
+        } // namespace MUSIC_ST
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_SIDECHAIN = 3336838409U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CROISSANCE = 2770033377U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID FX = 1802970371U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID OCEAN_VERB = 3878715091U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
